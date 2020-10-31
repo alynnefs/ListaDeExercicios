6d1739c101268e28f596c7aae3792f489a3af4d1
Robson Ribeiro <robsonribeiroft@gmail.com>
2020-10-31 15:58:22 -0300

    adding recursive exercise

---
+++ iniciante/solucoes/fibonacci_sequence/fibonacci_sequence.py
@@ -0,0 +1,11 @@
+def Fibonacci(n):
+    if n<=0:
+        print("invalid input")
+    elif n==1:
+        return 0
+    elif n==2:
+        return 1
+    else:
+        return Fibonacci(n-1)+Fibonacci(n-2)
+ 
+print(Fibonacci(9))
